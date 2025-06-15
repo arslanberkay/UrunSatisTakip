29644580e6348f03756b24fa015c2e65371f1529
Berkay Arslan <arslanberkay18@gmail.com>
2025-06-15 14:47:55 +0300

    Login Ekranı - Validation

---
+++ UrunStok.UI/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunStok.UI.ViewModel;
+
+namespace UrunStok.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminViewModel admin)
+        {
+            if (admin.KullaniciAdi == "admin" && admin.Sifre == "123")
+            {
+                return RedirectToAction("Index", "Satis");
+            }
+            TempData["Hata"] = "Kullanıcı Adı veya Şifre Hatalı";
+            return View();
+        }
+    }
+}
---
+++ UrunStok.UI/TagHelpers/LoginTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace UrunStok.UI.TagHelpers
+{
+    [HtmlTargetElement("custom-login-form")]
+    public class LoginTagHelper : TagHelper
+    {
+        public string HataMesaji { get; set; } //TagHelper içinde yazarken hata-mesaji olarak yazılır
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "form";
+            output.Attributes.SetAttribute("class", "custom-login-form");
+            output.Attributes.SetAttribute("method", "post");
+
+            var hataSpan = string.IsNullOrWhiteSpace(HataMesaji)
+                ? "<span class='text-danger'></span>" : $"<span class='text-danger'>{HataMesaji}</span>";
+
+            output.Content.SetHtmlContent(@$"
+<div class='form-group'>
+<label> Kullanıcı Adı </label>
+<input name = 'KullaniciAdi' required />
+</div>
+
+<div class='form-group'>
+<label> Şifre </label>
+<input name = 'Sifre' type='password'  required />
+</div>
+
+<button type='submit'> Giriş Yap </button>
+{hataSpan}");
+            base.Process(context, output);
+        }
+    }
+}
